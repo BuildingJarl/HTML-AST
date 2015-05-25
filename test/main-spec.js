4eac2ed19873c375c520bcb1616204a16c6dc3b9
BuildingJarl <BuildingJarl@users.noreply.github.com>
2015-05-25 16:13:25 +0100

    palying

--- test/main-spec.js
+++ test/main-spec.js
@@ -1,25 +1,133 @@
-import Parser from '../lib/main';
+import HtmlAst from '../lib/main';
+
 
 describe("", function() {
+	
+	it("", function() {
 
-	console.log('--------');
+		var html = '<html><head></head><body><div>Hello World</div></body></html>';
+		var ast = HtmlAst.toAst( html );
+	});
+})
+
+/*
+describe("calcRange", function() {
+
+	it("one line", function() {
+		var html = '<html><head></head><body></body></html>';
+
+		var actual = Parser.calcRange( html );
+
+		expect( actual.start ).toEqual( { row:0, col: 0 } );
+		expect( actual.end ).toEqual( { row:0, col: 38 } );
+	});
+
+	it("multiline", function() {
+		var html = '<html>\n<head></head>\n<body></body>\n</html>';
+
+		var actual = Parser.calcRange( html );
+
+		expect( actual.start ).toEqual( { row:0, col: 0 } );
+		expect( actual.end ).toEqual( { row:3, col: 6 } );
+	});
+
+	it("multiline + spaces", function() {
+		var html = '<html>\n    <head></head>\n        <body></body>\n    </html>';
+
+		var actual = Parser.calcRange( html );
+
+		expect( actual.start ).toEqual( { row: 0, col: 0 } );
+		expect( actual.end ).toEqual( { row: 3, col: 10 } );
+	});
+});
+
+describe("toAst", function() {
+	describe("", function() {
+		
+		it("1000", function() {
+                      //0123456789012345678901234567890123456789
+			var html = '<html><head></head><body></body></html>';
+			var actual = Parser.toAst(html);
+
+			var expected = [{
+				start: {
+					row: 0,
+					col: 0
+				},
+				end: {
+					row: 0,
+					col: 38
+				}
+			}, {
+				start: {
+					row: 0,
+					col: 6
+				},
+				end: {
+					row: 0,
+					col: 18
+				}
+			}, {
+				start: {
+					row: 0,
+					col: 19,
+				},
+				end: {
+					row: 0,
+					col: 31
+				}
+			}];
+
+			var i = 0;
+			Parser.traverse( actual, function(node) {
+				expect( node.location ).toEqual( expected[i] );
+				i++;
+			});
+		});
+	});
 
 	describe("", function() {
 		
 		it("1000", function() {
+                      //0123456789012345678901234567890123456789
+			var html = '<html>\n<head></head>\n<body></body>\n</html>';
+			var actual = Parser.toAst(html);
 
-			var html1 = '<open><one><oneone></oneone></one></open>';
-			var html2 = 'ishouldstart<open><one><oneone></oneone></one></open>ishouldend';
-
-			var actual1 = Parser.toAst(html1);
-			var actual2 = Parser.toAst(html2);
-
-			var expecter = [{
-
+			var expected = [{
+				start: {
+					row: 0,
+					col: 0
+				},
+				end: {
+					row: 3,
+					col: 6
+				}
+			}, {
+				start: {
+					row: 1,
+					col: 0
+				},
+				end: {
+					row: 1,
+					col: 12
+				}
+			}, {
+				start: {
+					row: 2,
+					col: 0,
+				},
+				end: {
+					row: 2,
+					col: 12
+				}
 			}];
 
-			expect(actual1.length).toBe(1);
-
+			var i = 0;
+			Parser.traverse( actual, function(node) {
+				expect( node.location ).toEqual( expected[i] );
+				i++;
+			});
 		});
 	});
 });
+*/
